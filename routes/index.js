d714167cbcd2a6f57f30a9394780e6ffb343a9bb
Bernardo <sd.bernardomartinez@gmail.com>
2022-11-20 15:53:20 -0600

    Fase 1 Terminado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page(index.ejs)*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TacoMex' });
+});
+
+/* GET home page(sobreNosotros.ejs)*/
+router.get('/views/Principal/sobreNosotros', function(req, res, next) {
+  res.render('../views/Principal/sobreNosotros', { title: 'Sobre nosotros' });
 });
 
 module.exports = router;
