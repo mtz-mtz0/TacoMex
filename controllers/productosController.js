2f95d97e867c06deb8d91061671248e7df94a1b4
Bernardo <sd.bernardomartinez@gmail.com>
2022-11-26 20:34:21 -0600

    Fase 2

---
+++ controllers/productosController.js
@@ -0,0 +1,5 @@
+module.exports={
+    productos : function name(rec,res){
+        res.render('../views/Administrador/Productos', { title: 'Productos' });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const productosController = require("../controllers/productosController");
 
 /* GET home page(index.ejs)*/
 router.get('/', function(req, res, next) {
@@ -10,5 +11,12 @@ router.get('/', function(req, res, next) {
 router.get('/views/Principal/sobreNosotros', function(req, res, next) {
   res.render('../views/Principal/sobreNosotros', { title: 'Sobre nosotros' });
 });
+
+/* GET home page(menu.ejs)*/
+router.get('/views/Principal/menu', function(req, res, next) {
+  res.render('../views/Principal/menu', { title: 'Menu' });
+});
+
+router.get('/views/Administrador/Productos', productosController.productos);
 
 module.exports = router;
