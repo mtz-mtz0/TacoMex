bac3614a94c71e32c9be4c409662193f9c89734c
Bernardo <sd.bernardomartinez@gmail.com>
2022-12-02 16:49:39 -0600

    controlador producto

---
+++ src/controllers/producto.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { ProductoModel } from "../models/productos";
+
+
+/* GET home page(editar_usuarios ejs)*/
+export async function indexViewProducto(req: Request, res: Response) {
+  return res.render('../views/productos/producto-view');
+}
+
+//muestra todos los usuarios
+//localhost:3000/api/producto 
+export async function readProducto(req: Request, res: Response) {
+  try {
+    const { query: where } = req
+    const productos = await ProductoModel.findAll({
+      attributes: [
+        "id_Producto", "nombre_pro", "direccion_pro", "id_fotografia_pro",
+        "precio_pro", "categoria_pro", "estatus_pro"],
+      raw: true,
+      where
+    });
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'habla con el administrador'
+    })
+  }
+}
+
+//insertar Producto
+export async function createProducto(req: Request, res: Response) {
+  try {
+    const { body } = req;
+    const productoResponse = await ProductoModel.create(body, { raw: true });
+    res.status(201).json(productoResponse);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'habla con el administrador'
+    })
+  }
+}
+
+//actualizar producto
+//localhost:3000/api/direccion/update/1
+export async function updateProducto(req: Request, res: Response) {
+
+  try {
+    const { id_Producto } = req.params;
+    const { body } = req;
+    const entity = await ProductoModel.findByPk(id_Producto);
+    await entity?.update(body);
+    res.status(201).json(entity?.toJSON());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'habla con el administrador'
+    })
+  }
+}
+
+//eliminar registros: Delete
+//localhost:3000/api/producto/2
+export async function deleteProducto(req: Request, res: Response) {
+  try {
+    const { id_Producto } = req.params;
+    const entity = await ProductoModel.findByPk(id_Producto);
+    await entity?.destroy();
+    res.status(204).json({ ok: "" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'habla con el administrador'
+    })
+  }
+}
