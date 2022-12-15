cb32526c99224e8d02cb1c1fdb115315f4876f5f
Abiu <callejas195@gmail.com>
2022-12-14 20:57:50 -0600

    refact: Despliegue final 14-12-22

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,16 @@
 import {Sequelize}from "sequelize";
 
-export const sequelize= new Sequelize('tacomex','root','password',{
-host:'localhost',
-dialect:'mysql',
-port: 3306,
-
-//dialectOptions:{
-//encrypt:true,},
-//logging: false,
+export const sequelize= new Sequelize('tacomex_sr65','tacomex_sr65_user','tGN6rs2YWLtv7kULWXkIKN7WHH29TfbC',{
+host:'dpg-ced4dqen6mpgskj6po70-a.frankfurt-postgres.render.com',
+dialect:'postgres',
+port: 5432,
+dialectOptions: {
+    encrypt: true,
+     ssl: {
+       rejectUnauthorized: false,
+     },
+  },
+  logging:false
 });
 
 export default sequelize; 
